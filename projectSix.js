f64f3b9e12a8a7f4218c579057d234436d1cc70c
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-02-20 01:51:35 +0600

    update

--- projectSix.js
+++ projectSix.js
@@ -22,22 +22,22 @@ const reviews = [
         name: 'Brother of Rose',
         type: 'flower',
         img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.indiamart.com%2Fproddetail%2Fdutch-rose-20537299391.html&psig=AOvVaw20D9fE2McrRdWc4MtZHngm&ust=1613845866136000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjnlsbL9u4CFQAAAAAdAAAAABAL',
-        text: 'I am a brother of Rose The rose is a type of flowering shrub. Its name comes from the Latin word Rosa.[1] The flowers of the rose grow in many different colors, from the well-known red rose or yellow roses and sometimes white or purple roses. Roses belong to the family of plants called Rosaceae. All roses were originally wild and they come from several parts of the world, North America, Europe, northwest Africa and many parts of Asia and Oceania. There are over 100 different species of roses. The wild rose species can be grown in gardens, but most garden roses are cultivars, which have been chosen by people.',
+        text: "I am a brother of Rose The rose is a type of flowering shrub. Its name comes from the Latin word Rosa.[1] The flowers of the rose grow in many different colors, from the well-known red rose or yellow roses and sometimes white or purple roses. Roses belong to the family of plants called Rosaceae. All roses were originally wild and they come from several parts of the world, North America, Europe, northwest Africa and many parts of Asia and Oceania. There are over 100 different species of roses. The wild rose species can be grown in gardens, but most garden roses are cultivars, which have been chosen by people.",
     },
     {
         id: 4,
         name: 'March',
         type: 'Alien',
-        img: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.ox.ac.uk%2Fsites%2Ffiles%2Foxford%2Fstyles%2Fow_medium_feature%2Fs3%2Ffield%2Ffield_image_main%2FAliens.jpg%3Fitok%3DkfbiuwSv&imgrefurl=https%3A%2F%2Fwww.ox.ac.uk%2Fnews%2F2017-10-31-aliens-may-be-more-us-we-think&tbnid=GFwzg2hFSMQKXM&vet=12ahUKEwjax6L-zPbuAhXI5TgGHY7iDrkQMygAegUIARCCAQ..i&docid=JtTEo2yih4YIUM&w=1200&h=575&q=alien&ved=2ahUKEwjax6L-zPbuAhXI5TgGHY7iDrkQMygAegUIARCCAQ',
-        text: 'Alien is a 1979 science fiction horror film directed by Ridley Scott and written by Dan O'Bannon. Based on a story by O'Bannon and Ronald Shusett, it follows the crew of the commercial space tug Nostromo, who encounter the eponymous Alien, an aggressive and deadly extraterrestrial set loose on the ship. The film stars Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton, John Hurt, Ian Holm, and Yaphet Kotto. It was produced by Gordon Carroll, David Giler, and Walter Hill through their company Brandywine Productions, and was distributed by 20th Century Fox. Giler and Hill revised and made additions to the script; Shusett was executive producer. The Alien and its accompanying artifacts were designed by the Swiss artist H. R. Giger, while concept artists Ron Cobb and Chris Foss designed the more human settings.',
+        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.ox.ac.uk%2Fsites%2Ffiles%2Foxford%2Fstyles%2Fow_medium_feature%2Fs3%2Ffield%2Ffield_image_main%2FAliens.jpg%3Fitok%3DkfbiuwSv&imgrefurl=https%3A%2F%2Fwww.ox.ac.uk%2Fnews%2F2017-10-31-aliens-may-be-more-us-we-think&tbnid=GFwzg2hFSMQKXM&vet=12ahUKEwjax6L-zPbuAhXI5TgGHY7iDrkQMygAegUIARCCAQ..i&docid=JtTEo2yih4YIUM&w=1200&h=575&q=alien&ved=2ahUKEwjax6L-zPbuAhXI5TgGHY7iDrkQMygAegUIARCCAQ",
+        text: "Alien is a 1979 science fiction horror film directed by Ridley Scott and written by Dan O'Bannon. Based on a story by O'Bannon and Ronald Shusett, it follows the crew of the commercial space tug Nostromo, who encounter the eponymous Alien, an aggressive and deadly extraterrestrial set loose on the ship. The film stars Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton, John Hurt, Ian Holm, and Yaphet Kotto. It was produced by Gordon Carroll, David Giler, and Walter Hill through their company Brandywine Productions, and was distributed by 20th Century Fox. Giler and Hill revised and made additions to the script; Shusett was executive producer. The Alien and its accompanying artifacts were designed by the Swiss artist H. R. Giger, while concept artists Ron Cobb and Chris Foss designed the more human settings.",
 
     },
     {
         id: 5,
         name: 'Earth',
-        type: 'Mercury',
-        img: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fg%2FMx4AAOSwlYRa40O6%2Fs-l300.jpg&imgrefurl=https%3A%2F%2Fwww.ebay.com%2Fitm%2FI-will-create-this-ALIEN-eye-sci-fi-space-video-intro-%2F263640767547&tbnid=I9TWwFKDJy6BuM&vet=12ahUKEwjj3-WBzvbuAhW87TgGHT9oA0AQMygVegUIARDDAQ..i&docid=kN0D3dmcjcbGWM&w=300&h=169&q=alien%20eye&ved=2ahUKEwjj3-WBzvbuAhW87TgGHT9oA0AQMygVegUIARDDAQ',
-        text: 'Alien is a 1979 science fiction horror film directed by Ridley Scott and written by Dan O'Bannon. Based on a story by O'Bannon and Ronald Shusett, it follows the crew of the commercial space tug Nostromo, who encounter the eponymous Alien, an aggressive and deadly extraterrestrial set loose on the ship. The film stars Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton, John Hurt, Ian Holm, and Yaphet Kotto. It was produced by Gordon Carroll, David Giler, and Walter Hill through their company Brandywine Productions, and was distributed by 20th Century Fox. Giler and Hill revised and made additions to the script; Shusett was executive producer. The Alien and its accompanying artifacts were designed by the Swiss artist H. R. Giger, while concept artists Ron Cobb and Chris Foss designed the more human settings.',
+        type: "Mercury",
+        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fg%2FMx4AAOSwlYRa40O6%2Fs-l300.jpg&imgrefurl=https%3A%2F%2Fwww.ebay.com%2Fitm%2FI-will-create-this-ALIEN-eye-sci-fi-space-video-intro-%2F263640767547&tbnid=I9TWwFKDJy6BuM&vet=12ahUKEwjj3-WBzvbuAhW87TgGHT9oA0AQMygVegUIARDDAQ..i&docid=kN0D3dmcjcbGWM&w=300&h=169&q=alien%20eye&ved=2ahUKEwjj3-WBzvbuAhW87TgGHT9oA0AQMygVegUIARDDAQ",
+        text: "Alien is a 1979 science fiction horror film directed by Ridley Scott and written by Dan O'Bannon. Based on a story by O'Bannon and Ronald Shusett, it follows the crew of the commercial space tug Nostromo, who encounter the eponymous Alien, an aggressive and deadly extraterrestrial set loose on the ship. The film stars Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton, John Hurt, Ian Holm, and Yaphet Kotto. It was produced by Gordon Carroll, David Giler, and Walter Hill through their company Brandywine Productions, and was distributed by 20th Century Fox. Giler and Hill revised and made additions to the script; Shusett was executive producer. The Alien and its accompanying artifacts were designed by the Swiss artist H. R. Giger, while concept artists Ron Cobb and Chris Foss designed the more human settings.",
 
     },
 ];
@@ -49,4 +49,43 @@ const Article = document.getElementById('info');
 const previousBtn = document.querySelector('.previous-button');
 const nextBtn = document.querySelector('.next-button');
 const randomBtn = document.querySelector('.random-button');
-console.log(image);
+
+
+let currentItem = 0;
+
+window.addEventListener("DOMContentLoaded", function(){
+
+    showPerson(currentItem);
+});
+
+
+function showPerson(person){
+    // const item = reviews[currentItem];
+    const item = reviews[person];
+    Name.textContent = item.name;
+    Type.textContent = item.type;
+    image.src = item.img;
+    Article.textContent = item.text;
+}
+
+nextBtn.addEventListener('click', function(){
+    currentItem++;
+    if(currentItem > reviews.length-1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+previousBtn.addEventListener('click', function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length -1;
+    }
+
+    showPerson(currentItem);
+});
+
+randomBtn.addEventListener('click', function(){
+    currentItem = Math.floor(Math.random()*reviews.length);
+    showPerson(currentItem);
+});
