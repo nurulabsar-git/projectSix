d1244438b48a3fa68247f5d58f1a143b792194ca
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-02-20 02:06:23 +0600

    update

--- projectSix.js
+++ projectSix.js
@@ -3,7 +3,7 @@ const reviews = [
         id: 1,
         name: 'Rose',
         type: 'Flower',
-        img: 'https://www.google.com/imgres?imgurl=https%3A%2F%2F5.imimg.com%2Fdata5%2FAA%2FKK%2FMY-6677193%2Fred-rose-500x500.jpg&imgrefurl=https%3A%2F%2Fwww.indiamart.com%2Fproddetail%2Fred-rose-16498244288.html&tbnid=PUD8obm8IYVY2M&vet=12ahUKEwiAxO29yvbuAhWF8DgGHR29CqEQMygFegUIARDOAQ..i&docid=X91vCL5rOKS3GM&w=500&h=500&q=rose&ved=2ahUKEwiAxO29yvbuAhWF8DgGHR29CqEQMygFegUIARDOAQ',
+        img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
         text: 'A rose is a woody perennial flowering plant of the genus Rosa, in the family Rosaceae, or the flower it bears.[1] There are over three hundred species and tens of thousands of cultivars.[1] They form a group of plants that can be erect shrubs, climbing, or trailing, with stems that are often armed with sharp prickles.[1] Flowers vary in size and shape and are usually large and showy, in colours ranging from white through yellows and reds.[1] Most species are native to Asia, with smaller numbers native to Europe, North America, and northwestern Africa.[1] Species, cultivars and hybrids are all widely grown for their beauty and often are fragrant. Roses have acquired cultural significance in many societies.[1] Rose plants range in size from compact, miniature roses, to climbers that can reach seven meters in height.[1] Different species hybridize easily, and this has been used in the development of the wide range of garden roses.[1]',
 
     },
@@ -12,7 +12,7 @@ const reviews = [
         id: 2,
         name: 'Sister of Rose',
         type: 'Flower',
-        img: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.charlotteflowermarket.com%2Fwp-content%2Fuploads%2F2018%2F03%2Frose-hot_pink-roseberry-1.jpg&imgrefurl=https%3A%2F%2Fwww.charlotteflowermarket.com%2Fproduct%2Frose-hot-pink-roseberry%2F&tbnid=_mjW3HdWU9MNMM&vet=12ahUKEwiAxO29yvbuAhWF8DgGHR29CqEQMygTegUIARDyAQ..i&docid=T-NFAFXEMbJvlM&w=1100&h=1100&q=rose&ved=2ahUKEwiAxO29yvbuAhWF8DgGHR29CqEQMygTegUIARDyAQ',
+        img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
         text: 'Sister of Rose A rose is a woody perennial flowering plant of the genus Rosa, in the family Rosaceae, or the flower it bears.[1] There are over three hundred species and tens of thousands of cultivars.[1] They form a group of plants that can be erect shrubs, climbing, or trailing, with stems that are often armed with sharp prickles.[1] Flowers vary in size and shape and are usually large and showy, in colours ranging from white through yellows and reds.[1] Most species are native to Asia, with smaller numbers native to Europe, North America, and northwestern Africa.[1] Species, cultivars and hybrids are all widely grown for their beauty and often are fragrant. Roses have acquired cultural significance in many societies.[1] Rose plants range in size from compact, miniature roses, to climbers that can reach seven meters in height.[1] Different species hybridize easily, and this has been used in the development of the wide range of garden roses.',
 
         
@@ -21,14 +21,14 @@ const reviews = [
         id: 3,
         name: 'Brother of Rose',
         type: 'flower',
-        img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.indiamart.com%2Fproddetail%2Fdutch-rose-20537299391.html&psig=AOvVaw20D9fE2McrRdWc4MtZHngm&ust=1613845866136000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjnlsbL9u4CFQAAAAAdAAAAABAL',
+        img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
         text: "I am a brother of Rose The rose is a type of flowering shrub. Its name comes from the Latin word Rosa.[1] The flowers of the rose grow in many different colors, from the well-known red rose or yellow roses and sometimes white or purple roses. Roses belong to the family of plants called Rosaceae. All roses were originally wild and they come from several parts of the world, North America, Europe, northwest Africa and many parts of Asia and Oceania. There are over 100 different species of roses. The wild rose species can be grown in gardens, but most garden roses are cultivars, which have been chosen by people.",
     },
     {
         id: 4,
         name: 'March',
         type: 'Alien',
-        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.ox.ac.uk%2Fsites%2Ffiles%2Foxford%2Fstyles%2Fow_medium_feature%2Fs3%2Ffield%2Ffield_image_main%2FAliens.jpg%3Fitok%3DkfbiuwSv&imgrefurl=https%3A%2F%2Fwww.ox.ac.uk%2Fnews%2F2017-10-31-aliens-may-be-more-us-we-think&tbnid=GFwzg2hFSMQKXM&vet=12ahUKEwjax6L-zPbuAhXI5TgGHY7iDrkQMygAegUIARCCAQ..i&docid=JtTEo2yih4YIUM&w=1200&h=575&q=alien&ved=2ahUKEwjax6L-zPbuAhXI5TgGHY7iDrkQMygAegUIARCCAQ",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
         text: "Alien is a 1979 science fiction horror film directed by Ridley Scott and written by Dan O'Bannon. Based on a story by O'Bannon and Ronald Shusett, it follows the crew of the commercial space tug Nostromo, who encounter the eponymous Alien, an aggressive and deadly extraterrestrial set loose on the ship. The film stars Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton, John Hurt, Ian Holm, and Yaphet Kotto. It was produced by Gordon Carroll, David Giler, and Walter Hill through their company Brandywine Productions, and was distributed by 20th Century Fox. Giler and Hill revised and made additions to the script; Shusett was executive producer. The Alien and its accompanying artifacts were designed by the Swiss artist H. R. Giger, while concept artists Ron Cobb and Chris Foss designed the more human settings.",
 
     },
@@ -36,7 +36,16 @@ const reviews = [
         id: 5,
         name: 'Earth',
         type: "Mercury",
-        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fg%2FMx4AAOSwlYRa40O6%2Fs-l300.jpg&imgrefurl=https%3A%2F%2Fwww.ebay.com%2Fitm%2FI-will-create-this-ALIEN-eye-sci-fi-space-video-intro-%2F263640767547&tbnid=I9TWwFKDJy6BuM&vet=12ahUKEwjj3-WBzvbuAhW87TgGHT9oA0AQMygVegUIARDDAQ..i&docid=kN0D3dmcjcbGWM&w=300&h=169&q=alien%20eye&ved=2ahUKEwjj3-WBzvbuAhW87TgGHT9oA0AQMygVegUIARDDAQ",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+        text: "Alien is a 1979 science fiction horror film directed by Ridley Scott and written by Dan O'Bannon. Based on a story by O'Bannon and Ronald Shusett, it follows the crew of the commercial space tug Nostromo, who encounter the eponymous Alien, an aggressive and deadly extraterrestrial set loose on the ship. The film stars Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton, John Hurt, Ian Holm, and Yaphet Kotto. It was produced by Gordon Carroll, David Giler, and Walter Hill through their company Brandywine Productions, and was distributed by 20th Century Fox. Giler and Hill revised and made additions to the script; Shusett was executive producer. The Alien and its accompanying artifacts were designed by the Swiss artist H. R. Giger, while concept artists Ron Cobb and Chris Foss designed the more human settings.",
+
+    },
+
+    {
+        id: 5,
+        name: 'Earth',
+        type: "Mercury",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
         text: "Alien is a 1979 science fiction horror film directed by Ridley Scott and written by Dan O'Bannon. Based on a story by O'Bannon and Ronald Shusett, it follows the crew of the commercial space tug Nostromo, who encounter the eponymous Alien, an aggressive and deadly extraterrestrial set loose on the ship. The film stars Tom Skerritt, Sigourney Weaver, Veronica Cartwright, Harry Dean Stanton, John Hurt, Ian Holm, and Yaphet Kotto. It was produced by Gordon Carroll, David Giler, and Walter Hill through their company Brandywine Productions, and was distributed by 20th Century Fox. Giler and Hill revised and made additions to the script; Shusett was executive producer. The Alien and its accompanying artifacts were designed by the Swiss artist H. R. Giger, while concept artists Ron Cobb and Chris Foss designed the more human settings.",
 
     },
